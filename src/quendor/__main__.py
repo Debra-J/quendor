f776bf1d4750be8932c9b27fc14357464d982c24
Jeff Nyman <jeffnyman@gmail.com>
2021-12-09 06:39:20 -0600

    Providing an executable package.

---
+++ src/quendor/__main__.py
@@ -0,0 +1,15 @@
+"""Package entry point for the Quendor interpreter."""
+
+import sys
+
+
+def main() -> int:
+    """Entry point function for the Quendor interpreter."""
+
+    print("\nQuendor Z-Machine Interpreter\n")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- tests/test_quendor.py
+++ tests/test_quendor.py
@@ -1,6 +1,8 @@
 """Generic tests for Quendor execution."""
 
-from expects import equal, expect
+from expects import contain, equal, expect
+
+import pytest
 
 
 def test_report_package_version() -> None:
@@ -9,3 +11,16 @@ def test_report_package_version() -> None:
     import quendor
 
     expect(quendor.__version__).to(equal("0.1.0"))
+
+
+def test_quendor_startup_banner(capsys: pytest.CaptureFixture) -> None:
+    """Quendor provides a minimal banner."""
+
+    from quendor.__main__ import main
+
+    main()
+
+    captured = capsys.readouterr()
+    result = captured.out
+
+    expect(result).to(contain("Quendor Z-Machine Interpreter"))
